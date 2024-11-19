d8006edd202630877ef5ae6b26b442db4b807d2e
Pon Lwin <114682565+PonNyeMinLwin@users.noreply.github.com>
2024-11-19 02:16:26 +0000

    Added the player character and a simple movement script

---
+++ ThiefSimulator/Assets/Scripts/Characters/RobberController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobberController : MonoBehaviour
+{
+    public GameObject playerCharacter;
+    public bool isMoving;
+    public float horizontalMovement;
+    public float verticalMovement;
+
+    void Update() {
+        //Making the character move with their animations 
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            playerCharacter.GetComponent<Animation>().Play("Run");
+            horizontalMovement = Input.GetAxis("Horizontal") * Time.deltaTime * 150;
+            verticalMovement = Input.GetAxis("Vertical") * Time.deltaTime * 8;
+            isMoving = true;
+            transform.Rotate(0, horizontalMovement, 0);
+            transform.Translate(0, 0, verticalMovement);
+        }
+        else {
+            playerCharacter.GetComponent<Animation>().Play("Ninja Idle");
+            isMoving = false;
+        }
+    }
+}
